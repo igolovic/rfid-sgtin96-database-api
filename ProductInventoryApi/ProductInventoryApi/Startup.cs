312f639ef10bea7dad1c629b9fe397182e80e83e
Ivan Golović <igolovic@gmail.com>
2021-05-22 02:35:35 +0200

    Implementation of publish. Creation of Postman test data for azure URL.

--- ProductInventoryApi/ProductInventoryApi/Startup.cs
+++ ProductInventoryApi/ProductInventoryApi/Startup.cs
@@ -69,12 +69,9 @@ namespace ProductInventoryApi
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ProductInventoryApi v1"));
-            }
+            app.UseDeveloperExceptionPage();
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ProductInventoryApi v1"));
 
             // Manage returned error message (e.g. caused by exception) - it shouldn't reveal technical details,
             // must be called before UseRouting (?)
